96b6a6795e021c0c1ec748e043b08d9df7e27f7d
plunyo <conradthieu87@gmail.com>
2025-09-29 22:09:17 +1000

    NOAH EXPORT IT URSELF GNG

--- scripts/player_spawner.gd
+++ scripts/player_spawner.gd
@@ -10,48 +10,49 @@ func _ready() -> void:
 func _on_received_packet(packet_id: int, data: PackedByteArray) -> void:
 	match packet_id:
 		ServerConnection.PacketID.Incoming.UPDATE_PLAYERS:
-			for player_data in ServerConnection.read_player_update(data, 0).value:
-				var id = player_data.get("id")
-				if id == ServerConnection.client_id:
-					continue
-
-				var pos = player_data.get("position", Vector2.ZERO)
-
-				if id in players:
-					# update existing player smoothly
-					players[id].target_position = pos
-				else:
-					# create new player instance
-					var new_player := PLAYER_SCENE.instantiate() as Player
-					new_player.id = id
-					new_player.global_position = pos
-					new_player.target_position = pos
-					players[id] = new_player
-					add_child(new_player)
-					new_player.camera.enabled = id == ServerConnection.client_id
-
+			_handle_player_update(ServerConnection.read_player_update(data, 0).value)
 		ServerConnection.PacketID.Incoming.SYNC_PLAYERS:
-			for player_data in ServerConnection.read_player_sync(data, 0).value:
-				var id = player_data.get("id")
-				if id == null:
-					continue
+			_handle_player_sync(ServerConnection.read_player_sync(data, 0).value)
 
-				var username = player_data.get("username", "Anonymous")
-				var net_position = player_data.get("position", Vector2.ZERO)
+# helper to create a new player
+func _create_player(id: int, net_position: Vector2, username: String = "Anonymous") -> Player:
+	var player := PLAYER_SCENE.instantiate() as Player
+	player.id = id
+	player.username = username
+	player.global_position = net_position
+	player.target_position = net_position
+	player.camera.enabled = id == ServerConnection.client_id
+	add_child(player)
+	players[id] = player
+	return player
 
-				if id in players:
-					# update existing player
-					var existing := players[id]
-					existing.username = username
-					if id != ServerConnection.client_id:
-						existing.target_position = net_position
-				else:
-					# create new player instance
-					var player_instance := PLAYER_SCENE.instantiate() as Player
-					player_instance.id = id
-					player_instance.username = username
-					player_instance.global_position = net_position
-					player_instance.target_position = net_position
-					players[id] = player_instance
-					add_child(player_instance)
-					player_instance.camera.enabled = id == ServerConnection.client_id
+# handle UPDATE_PLAYERS packet
+func _handle_player_update(player_list: Array) -> void:
+	for data in player_list:
+		var id = data.get("id")
+		if id == ServerConnection.client_id:
+			continue
+
+		var pos = data.get("position", Vector2.ZERO)
+		if id in players:
+			players[id].target_position = pos
+		else:
+			_create_player(id, pos)
+
+# handle SYNC_PLAYERS packet
+func _handle_player_sync(player_list: Array) -> void:
+	for data in player_list:
+		var id = data.get("id")
+		if id == null:
+			continue
+
+		var username = data.get("username", "Anonymous")
+		var pos = data.get("position", Vector2.ZERO)
+
+		if id in players:
+			var existing := players[id]
+			existing.username = username
+			if id != ServerConnection.client_id:
+				existing.target_position = pos
+		else:
+			_create_player(id, pos, username)
